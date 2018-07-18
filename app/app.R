5c6d1750fed9e9a2a4c785a0f9ca846950f3c71e
nickp60 <nickp60@gmail.com>
2018-07-18 12:06:47 +0100

    update shiny model creation

--- app/app.R
+++ app/app.R
@@ -14,7 +14,9 @@ X <- NA
 ORIGINALX <- X
 
 COLS <- NA
-X <-  read.csv("./testdata.csv",
+sPAIRS <- NA
+PAIRS <- NA
+X <-  read.csv("./data/testdata.csv",
                header = T,
                sep = ",",
                quote = "\"")
@@ -32,41 +34,98 @@ updateX <- function(input=input){
   #   }
   # }
   X <<- X
+  sPAIRS <<- paste(expand.grid(COLS, COLS)$Var1, expand.grid(COLS, COLS)$Var2)
+  PAIRS <<- strsplit(sPAIRS, " ")
+  
   # str(X)
 }
+updateX()
 
+make_model<- function(x, y, random=c(), fixed=c(), interacting=list(), nested=list()){
+  # make a model given a few parameters
+  # interacting parameters should be a list of vector pairs, like list(c("A", "B"), c("B", "C"))[
+  randoms = ""
+  fixeds = ""
+  interactings = ""
+  nesteds = ""
+  fixed = fixed[x != fixed]  # remove x from fixed effects, if its in there
+  
+  mx <- paste(y, "~", x)
+  
+  if (length(fixed) != 0){
+    fixeds <- paste(fixed, collapse = " + ")
+    mx <- paste(mx, fixeds, sep = " + " )
+  }
+  if (length(random) != 0){
+    randoms <- paste("(1", "|", random, ")", collapse = " + ")
+    mx <- paste(mx, randoms, sep = " + " )
+  }
+  if (length(interacting) != 0){
+    interaction_strings = c()
+    for (i in 1:length(interacting)){
+      interaction_strings <- c(
+        interaction_strings, 
+        paste("(", interacting[[i]][1], "*",  interacting[[i]][2], ")" )
+      )
+    }
+    interactings <- paste(interaction_strings, collapse = " + ")
+    mx <- paste(mx, interactings, sep = " + " )
+  }
+  if (length(nested) != 0){
+    nested_strings = c()
+    for (i in 1:length(nested)){
+      nested_strings <- c(
+        nested_strings, 
+        paste("(", nested[[i]][1], "/",  nested[[i]][2], ")" )
+      )
+    }
+    nesteds <- paste(nested_strings, collapse = " + ")
+    mx <- paste(mx, nested_strings, sep = " + " )
+  }
+  return(mx)
+}
 # Define UI for application that draws a histogram
 ui <- fluidPage(
    
-   # Application title
-   titlePanel("Analysis of Process Data"),
-   # Input: Select a file ----
-   # Sidebar with a slider input for number of bins 
-   fluidRow(
-     column( 3, 
-   # sidebarLayout(
-      #sidebarPanel(
-        fileInput("file1", "Choose CSV File",
-                  multiple = FALSE,
-                  accept = c("text/csv",
-                             "text/comma-separated-values,text/plain",
-                             ".csv")),
-        
-        # Horizontal line ----
-        tags$hr(),
-        actionButton("add", "Add UI") ,  
-        tags$hr()
-        
+  # Application title
+  titlePanel("Analysis of Process Data"),
+  # Input: Select a file ----
+  # Sidebar with a slider input for number of bins 
+  sidebarLayout(
+    sidebarPanel(
+      fileInput(
+        "file1", "Choose CSV File",
+        multiple = FALSE,
+        accept = c("text/csv",
+                   "text/comma-separated-values,text/plain",
+                   ".csv")
       ),
-      
+      tags$hr(),
+      selectInput("xcol", "X column/Independent Variable", choices=COLS, selected=COLS[1]),
+      selectInput("ycol", "y column/Dependent Variable", choices=COLS, selected = COLS[5]),
+      selectInput("groupcol", "group column", choices=COLS, selected = COLS[2]),
+      checkboxGroupInput("fixedcols", "Fixed Effects", choices = COLS),
+      selectInput("randomcols", "Random Effects", choices = sPAIRS, multiple = T),
+      selectInput("interactingcols", "Interacting Effects", choices = sPAIRS, multiple=T),
+      #      checkboxGroupInput("Nestedcols", "Interating Effects", choices = COLS)
+      actionButton("add", "Update Model")
+    ),
+    
       # Show a plot of the generated distribution
-      column(4,
-         dataTableOutput("contents")
-      ),
-   column(4, offset = 1,
+    mainPanel(
+      tabsetPanel(
+        tabPanel(
+          "Plot", 
+          dataTableOutput("contents"),
           plotOutput("exploreplot")
+        ),
+        tabPanel(
+          "model",
+          textOutput("model")
+        )
       )
-   )
+    )
+  )
 )
 
 # Define server logic required to draw a histogram
@@ -110,36 +169,49 @@ server <- function(input, output) {
    },   options = list(
      pageLength = 5)
    )
-   observeEvent(input$add, {
+   # observeEvent(input$add, {
      # for (col in COLS)
      # insertUI(
      #   selector = "#add",
      #   where = "afterEnd",
      #   ui = checkboxGroupInput("factorcols", "Columns that should be treated as factors (ie, non-numeric)", choices = COLS)
      # )
-     insertUI(
-       selector = "#add",
-       where = "afterEnd",
-       ui =tags$hr()
-     )
-     insertUI(
-       selector = "#add",
-       where = "afterEnd",
-       ui = selectInput("groupcol", "group column", choices=colnames(X), selected =colnames(X)[3])
-     )
-     insertUI(
-       selector = "#add",
-       where = "afterEnd",
-       ui = selectInput("ycol", "y column", choices=colnames(X), selected = colnames(X)[2])
-     )
-     insertUI(
-       selector = "#add",
-       where = "afterEnd",
-       ui = selectInput("xcol", "x column", choices=colnames(X), selected=colnames(X)[1])
-     )
-   })
+     #shinyjs::disable("add")
+   #   insertUI(
+   #     selector = "#add",
+   #     where = "afterEnd",
+   #     ui = checkboxGroupInput("fixedcols", "Fixed Effects", choices = COLS, selected = input$ycol)
+   #   )
+   #   insertUI(
+   #     selector = "#add",
+   #     where = "afterEnd",
+   #     ui =tags$hr()
+   #   )
+   #   insertUI(
+   #     selector = "#add",
+   #     where = "afterEnd",
+   #     ui = selectInput("groupcol", "group column", choices=colnames(X), selected =colnames(X)[3])
+   #   )
+   #   insertUI(
+   #     selector = "#add",
+   #     where = "afterEnd",
+   #     ui = selectInput("ycol", "y column/Dependent Variable", choices=colnames(X), selected = colnames(X)[2])
+   #   )
+   #   insertUI(
+   #     selector = "#add",
+   #     where = "afterEnd",
+   #     ui = selectInput("xcol", "X column/Independent Variable", choices=colnames(X), selected=colnames(X)[1])
+   #   )
+   # })
+   ###################################################################
+   observeEvent(input$add, {
    
+   output$model <- renderText(
+     make_model(x=input$xcol,  y=input$ycol, random=c(), fixed=input$fixedcols,
+                interacting=list(), nested=list())
+   )
    #output$ui <- renderUI(checkboxInput('test', 'checkboxes', colnames(input$file1)))
+   })
    
 }
 
