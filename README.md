698c941903f53bf9ab714af4889d8a943605356c
nickp60 <nickp60@gmail.com>
2018-07-19 15:56:10 +0100

    remove shape option; life's too short

--- README.md
+++ README.md
@@ -1,26 +1,3 @@
-# Mixed Effects Linear modelling for Time Series
-
-# Overview
-Meltsy is an easy interface for exploring mixed models.  For a nice introduction to the technique, check out [this tutorial](http://bodowinter.com/tutorial/bw_LME_tutorial.pdf).  Meltsy allows you to upload your data, make some preliminary plots, build models, run differnt models, and compare models for statistical significance.
-
-# Caveats
-This is supposed to be the "gateway drug" towards performing robust mixed models analysis.  It takes some of the work out of running the models, but it is up to the user to determine the validity of the models.  This does no work to check whether your model fits well or not, so be cautious.  That said, have fun!
-
-
-# Data
-By default, test data is loaded. If you want to upload your own data, it needs to be formatted correctly.
-
-1. Each row should represent the data of one subject/timepoint (ie, a unique combination of all independent variables), and can have multiple response variables (wide data). 
-2. All values that can be treated as number will be, so adjust the data appropriately (ie, a column with replicates codes as 1 2 3 should be changed to A B C etc). 
-3. data should be in csv format. Upload your data using the button on the right.
-
-# Plots
-Meltsy allows you to make some basic plots of your data. Adding fixed effects adds color groupings and other things. If you want anything more complex, it will be worth your time learning some R.  There are loads of good tutorials online.
-
-# Model
-The 'model' tab shows the model which will later be used by the lme4 package. Hitting the `Evaluate Model` button runs the model and displays the results. If you have no random effects, a simple linear model will be run. See this doc for some info on interpretting the results: http://lme4.r-forge.r-project.org/book/Ch1.pdf. Hitting the `Save Model` button records the model and the parameters so that you can perfom comparisons later.
-
-# Model 
-Statistical significance is determined by running an ANOVA between two models.  Say you are interested whether in this case 'treatment' effects the methane by day, you would compare the model `methane ~ day` and `methane ~ day + treatment`.
+# See readme in `app` dir
 
 
--- app/app.R
+++ app/app.R
@@ -1,26 +1,15 @@
 library(shiny)
+library(markdown)
 library(ggplot2)
 library(lme4)
 
 # look global variables cause I'm a lazy duck
-X <- NA
-ORIGINALX <- X
-MODEL = NA
-COLS <- NA
-sPAIRS <- NA
-PAIRS <- NA
+
 X <-  read.csv("./data/testdata.csv",
                header = T,
                sep = ",",
                quote = "\"")
 
-updateX <- function(){
-  # This is a terrible function to update global variables X and cols
-  COLS <<- colnames(X)
-  sPAIRS <<- paste(expand.grid(COLS, COLS)$Var1, expand.grid(COLS, COLS)$Var2)
-  PAIRS <<- strsplit(sPAIRS, " ")
-}
-updateX()
 
 make_model<- function(x, y, random=c(), fixed=c(), interacting=list(), nested=list()){
   # make a model given a few parameters
@@ -102,20 +91,21 @@ ui <- fluidPage(
       tabsetPanel(
         tabPanel(
           "?. Help", 
-          includeMarkdown("../README.md")
+          includeMarkdown("./README.md")
           
         ),
         tabPanel(
           "1. Plot", 
-          dataTableOutput("contents")
-#          plotOutput("exploreplot")
+          dataTableOutput("contents"),
+          plotOutput("exploreplot"),
+          verbatimTextOutput("plot_errors")
         ),
         tabPanel(
           "2. Model",
           verbatimTextOutput("model"),
           actionButton("A_execute_model", "Execute Model"),
           verbatimTextOutput("model_output"),
-          actionButton("save_model", "Save Model"),
+          uiOutput("dy_model_save"),
           dataTableOutput("modelsdf")
         ),
         tabPanel(
@@ -135,33 +125,36 @@ ui <- fluidPage(
 # Define server logic required to draw a histogram
 server <- function(input, output) {
   values <- reactiveValues(DF=NA, PAIRS=NA, RESULTS = data.frame(), RESULTS_COUNTER=0, MODEL=NA, MODEL_STRING=NA, MODEL_LIST=list())
-  output$exploreplot <- renderPlot({
-    req(values$DF)
-    # print(input$fixedcols)
-    # print(length(input$fixedcols))
-    # print(input$fixedcols[1])
-    try(
-      if(is.null(input$fixedcols)){
-        ggplot(values$DF, aes_string(y=input$ycol, x=input$xcol))+
-          geom_smooth(alpha=.2)+ 
-          geom_jitter(width = .1)
-      } else if(length(input$fixedcols) == 1){
-        ggplot(values$DF, aes_string(y=input$ycol, x=input$xcol, color=input$fixedcols[1], shape=input$fixedcols[1]))+
-          geom_smooth(alpha=.2)+ 
-          geom_jitter(width = .1)
-      } else if(length(input$fixedcols) == 2){
-        ggplot(values$DF, aes_string(y=input$ycol, x=input$xcol, color=input$fixedcols[1], linetype=input$fixedcols[2], shape=input$fixedcols[1]))+
-          geom_smooth(alpha=.2)+ 
-          geom_jitter(width = .1)
-      } else if(length(input$fixedcols) == 3){
-        ggplot(values$DF, aes_string(y=input$ycol, x=input$xcol, color=input$fixedcols[1], fill=input$fixedcols[2], linetype=input$fixedcols[3], shape=input$fixedcols[1]))+
-          geom_smooth(alpha=.2)+ 
-          geom_jitter(width = .1)
-      }
-      
-    )
-    
-  })
+  tryCatch(
+    {
+      output$exploreplot <- renderPlot({
+        req(values$DF)
+        # print(input$fixedcols)
+        # print(length(input$fixedcols))
+        # print(input$fixedcols[1])
+        if(is.null(input$fixedcols)){
+          ggplot(values$DF, aes_string(y=input$ycol, x=input$xcol))+
+            geom_smooth(alpha=.2)+ 
+            geom_jitter(width = .1)
+        } else if(length(input$fixedcols) == 1){
+          ggplot(values$DF, aes_string(y=input$ycol, x=input$xcol, color=input$fixedcols[1]))+
+            geom_smooth(alpha=.2)+ 
+            geom_jitter(width = .1)
+        } else if(length(input$fixedcols) == 2){
+          ggplot(values$DF, aes_string(y=input$ycol, x=input$xcol, color=input$fixedcols[1], linetype=input$fixedcols[2]))+
+            geom_smooth(alpha=.2)+ 
+            geom_jitter(width = .1)
+        } else if(length(input$fixedcols) == 3){
+          ggplot(values$DF, aes_string(y=input$ycol, x=input$xcol, color=input$fixedcols[1], fill=input$fixedcols[2], linetype=input$fixedcols[3]))+
+            geom_smooth(alpha=.2)+ 
+            geom_jitter(width = .1)
+        }
+      })
+    }, error = function(e) {
+      # return a safeError if a parsing error occurs
+      output$plot_errors <- renderPrint(safeError(e))
+    }
+  )
   rdataframe <- reactive({
     # input$file1 will be NULL initially. After the user selects
     # and uploads a file, head of that data file by default,
@@ -169,7 +162,7 @@ server <- function(input, output) {
     if (is.null(input$userfile)){
       print("loading test data")
       values$DF <- X
-      updateX()
+      # updateX()
       return(values$DF)
     } else{
       df <- read.csv(input$userfile$datapath,
@@ -179,7 +172,7 @@ server <- function(input, output) {
       
       print("loading user data!")
       values$DF <- df
-      updateX()
+      # updateX()
       return(df)
       
     }
@@ -213,7 +206,15 @@ server <- function(input, output) {
   output$dy_interactingcols <- renderUI({
     selectInput("interactingcols", "Interacting Effects", choices = rspairs(), multiple=T)
   })
-  
+  output$dy_model_save <- renderUI({
+    print(values$MODEL)
+    if (is.na(values$MODEL)){
+      return(NULL)
+    }else {
+      actionButton("save_model", "Save Model")
+    }
+  })
+
   
   output$contents <- renderDataTable({
     rdataframe()
@@ -323,14 +324,38 @@ server <- function(input, output) {
     values$RESULTS
   })
   observeEvent(input$run_anova, {
+    resp1 <- values$RESULTS[[as.numeric(input$model1), "ycol"]]
     model1 <-  values$MODEL_LIST[[as.numeric(input$model1)]]
+    print(model1)
+    print(resp1)
+    resp2 <- values$RESULTS[[as.numeric(input$model2), "ycol"]]
     model2 <-  values$MODEL_LIST[[as.numeric(input$model2)]]
-    aov <- anova(model1, model2)
-    print(aov)
-    
-    output$model_compare_output <- renderPrint({
-      return(aov)
-    })
+    print(model2)
+    print(resp2)
+    # dont try to compare linear models this way
+    if (nchar(values$RESULTS[[as.numeric(input$model1), "randomcols"]]) == 0 &&
+        nchar(values$RESULTS[[as.numeric(input$model1), "randomcols"]]) == 0){
+      output$model_compare_output <- renderPrint({
+        "Error: can't compare non-mixed models currently"
+      })
+    } else if (resp1 == resp2){
+      # make sure we are comparing the same response variable
+      tryCatch(
+        {
+          aov <- anova(model1, model2)
+          print(aov)
+          output$model_compare_output <- renderPrint({
+            return(aov)
+          })
+        }, error = function(e) {
+          # return a safeError if a parsing error occurs
+          output$model_compare_output <- renderPrint(safeError(e))
+        }
+      )
+    } else {
+      output$model_compare_output <- renderPrint(
+        "Error: Response variable not the same between models!")
+    }
   })
 }
 
