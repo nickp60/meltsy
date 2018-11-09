7383e2fcec294f913cbe29fd45ea4a382531d070
nickp60 <nickp60@gmail.com>
2018-11-09 17:26:51 +0000

    change ggplot to ggplot2

--- install_deps.R
+++ install_deps.R
@@ -1,6 +1,6 @@
 #!/usr/bin/Rscript
 
-for (lib in c("shiny", "markdown", "ggplot", "lme4")){
+for (lib in c("shiny", "markdown", "ggplot2", "lme4")){
     if(! lib  %in% rownames(installed.packages())) {
         print(paste("Installing", lib) )
         install.packages(lib, repos='http://cran.us.r-project.org')
